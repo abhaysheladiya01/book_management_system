331a4422f966d8c299ea4fbe379509a6a39debc7
abhaysheladiya01 <abhay.sheladiya@seaflux.tech>
2026-02-09 12:42:10 +0530

    fix: Update database password in databse.js

--- util/databse.js
+++ util/databse.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'abhay',
     database: 'node_complete',
-    password: '8227'
+    password: 'put_passworld'
 });
 
 module.exports = pool.promise();
