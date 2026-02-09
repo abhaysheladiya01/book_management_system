c39bfab6353b70c252faec1918dc267658ae14db
Abhay <abhay.sheladiya@seaflux.tech>
2026-02-09 14:55:36 +0530

    feat:  Refactor product management to use MySQL database and update cart functionality.

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'abhay',
     database: 'node_complete',
-    password: '8227'
+    password: 'pswd'
 });
 
 module.exports = pool.promise();
