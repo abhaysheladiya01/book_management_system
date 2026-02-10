c2129076c23e6959c0b0d01cb6c2d1931003af63
Abhay <abhay.sheladiya@seaflux.tech>
2026-02-10 09:45:54 +0530

    feat: implement Sequelize models for cart, order, and product management
    - Added CartItem model to represent items in the cart.
    - Refactored Cart model to use Sequelize instead of file-based storage.
    - Introduced Order and OrderItem models for order management.
    - Updated Product model to utilize Sequelize for database interactions.
    - Modified database connection to use Sequelize with MySQL.
    - Enhanced cart view with CSS styling and order functionality.
    - Updated routes to handle order creation and display orders.
    - Improved order view to list products in each order.

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'abhay',
-    database: 'node_complete',
-    password: 'put_passworld'
+const sequelize = new Sequelize('node_complete', 'abhay', 'your_pswd', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
